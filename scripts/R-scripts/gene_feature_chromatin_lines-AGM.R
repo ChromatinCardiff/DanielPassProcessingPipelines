b1ee4b84ef0ba2da1cbf1798c3ca5b8e9a109fd6
passdan <daniel.antony.pass@googlemail.com>
2016-04-15 17:55:57 +0100

    heatmaps

---
+++ scripts/R-scripts/gene_feature_chromatin_lines-AGM.R
@@ -0,0 +1,193 @@
+library(ggplot2)
+library(data.table)
+library(scales)
+library(reshape2)
+library(plyr)
+library(vegan)
+
+####################################################################################################################
+###  BASIC CHART  ##################################################################################################
+####################################################################################################################
+
+# Read in
+TSS <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_TSS.xls", header=TRUE, sep="\t")
+TTS <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_TTS.xls", header=TRUE, sep="\t")
+CSS <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_CSS.xls", header=TRUE, sep="\t")
+CTS <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_CTS.xls", header=TRUE, sep="\t")
+ESS <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_ESS.xls", header=TRUE, sep="\t")
+ETS <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_ETS.xls", header=TRUE, sep="\t")
+gene <- read.table("/home/sbi6dap/Projects/AGM/MNaseseq/danposProfileFull/mean_full_gene.xls", header=TRUE, sep="\t")
+
+#other
+TSSA <- read.table("/home/sbi6dap/Projects/ALD/MNase-seq/dpos_peaks-RNA-guided/mean_ARA11/mean_CSS.xls", header=TRUE, sep="\t", row.names=1)
+TSST <- read.table("/home/sbi6dap/Projects/ALD/MNase-seq/dpos_peaks-RNA-guided/mean_TAIR/mean_profile_CSS.xls", header=TRUE, sep="\t", row.names=1)
+
+#tmp
+tmp <- read.table("/home/sbi6dap/Projects/ALD/totalcoverage/mean_ESS_heatmap/LDcommonmodel-ARA11.genepred.ess.ES09_totalcov.Fnor.wig.heatmap_1st-exon.xls.cut", header=TRUE, sep="\t", row.names=1)
+
+# Annotations
+Exposure <- c("Light","Light","Dark","Dark","Light","Light","Dark","Dark")
+Treatment <- c("Low","High","Low","High","Low","High","Low","High")
+
+############### TSS ###############
+
+TSSA.dec <- as.data.frame(decostand(t(TSSA), 'standardize', MARGIN=1))
+Annot <- c("ARA","ARA","ARA","ARA","ARA","ARA","ARA","ARA")
+TSSA.dec$pos <- rownames(TSSA.dec)
+TSSA.dec = cbind(Exposure,Treatment,Annot,TSSA.dec)
+TSSA.melt <- melt(TSSA.dec, id=c("pos","Exposure","Treatment", "Annot"))
+
+TSST.dec <- as.data.frame(decostand(t(TSST), 'standardize', MARGIN=1))
+Annot <- c("TAIR","TAIR","TAIR","TAIR","TAIR","TAIR","TAIR","TAIR")
+TSST.dec$pos <- rownames(TSST.dec)
+TSST.dec = cbind(Exposure,Treatment,Annot,TSST.dec)
+TSST.melt <- melt(TSST.dec, id=c("pos","Exposure","Treatment", "Annot"))
+
+#combine melts
+TSSall.melt <- rbind(TSST.melt,TSSA.melt)
+ESSall.melt <- rbind(ESST.melt,ESSA.melt)
+
+# Chart all columns
+p <-ggplot(data=TSSA.melt, aes(x=as.numeric(as.character(variable)), y=value, colour=Exposure, lty=Annot))
+TSS.plot <-p +
+  stat_smooth(method="loess", span=0.01, se=FALSE) + 
+  #geom_line(aes(x=as.numeric(as.character(variable)), y=value))+#, colour=Exposure)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  #scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "bottom") +
+  labs(title = "TSS") +
+  facet_wrap(~ Treatment, ncol=1)
+TSS.plot
+############### TTS ###############
+# Processing
+rownames(TTS) <-TTS$pos
+TTS.dec <- decostand(TTS, 'standardize', MARGIN=2)
+TTS.dec$pos <- as.numeric(rownames(TTS))
+TTS.melt <- melt(TTS.dec, id=c("pos"))
+
+# Chart all columns
+p <-ggplot(data=TTS.melt)
+TTS.plot <-p +
+  geom_line(aes(x=pos, y=value, colour=variable)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "none") +
+  labs(title = "TTS")
+
+############### CSS ###############
+# Processing
+rownames(CSS) <-CSS$pos
+CSS.dec <- decostand(CSS, 'standardize', MARGIN=2)
+CSS.dec$pos <- as.numeric(rownames(CSS))
+CSS.melt <- melt(CSS.dec, id=c("pos"))
+
+# Chart all columns
+p <-ggplot(data=CSS.melt)
+CSS.plot <-p +
+  geom_line(aes(x=pos, y=value, colour=variable)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "none") +
+  labs(title = "CSS")
+
+############### CTS ###############
+# Processing
+rownames(CTS) <-CTS$pos
+CTS.dec <- decostand(CTS, 'standardize', MARGIN=2)
+CTS.dec$pos <- as.numeric(rownames(CTS))
+CTS.melt <- melt(CTS.dec, id=c("pos"))
+
+# Chart all columns
+p <-ggplot(data=CTS.melt)
+CTS.plot <-p +
+  geom_line(aes(x=pos, y=value, colour=variable)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "none") +
+  labs(title = "CTS")
+
+############### ESS ###############
+# Processing
+rownames(ESS) <-ESS$pos
+ESS.dec <- decostand(ESS, 'standardize', MARGIN=2)
+ESS.dec$pos <- as.numeric(rownames(ESS))
+ESS.melt <- melt(ESS.dec, id=c("pos"))
+
+# Chart all columns
+p <-ggplot(data=ESS.melt)
+ESS.plot <-p +
+  geom_line(aes(x=pos, y=value, colour=variable)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "none") +
+  labs(title = "ESS")
+
+############### ETS ###############
+# Processing
+rownames(ETS) <-ETS$pos
+ETS.dec <- decostand(ETS, 'standardize', MARGIN=2)
+ETS.dec$pos <- as.numeric(rownames(ETS))
+ETS.melt <- melt(ETS.dec, id=c("pos"))
+
+# Chart all columns
+p <-ggplot(data=ETS.melt)
+ETS.plot <-p +
+  geom_line(aes(x=pos, y=value, colour=variable)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "none") +
+  labs(title = "ETS")
+
+############### gene ###############
+# Processing
+rownames(gene) <-gene$pos
+gene.dec <- decostand(gene, 'standardize', MARGIN=2)
+gene.dec$pos <- as.numeric(rownames(gene))
+gene.melt <- melt(gene.dec, id=c("pos"))
+
+# Chart all columns
+p <-ggplot(data=gene.melt)
+gene.plot <-p +
+  geom_line(aes(x=pos, y=value, colour=variable)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  geom_vline(x=0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "none") +
+  labs(title = "gene")
+
+multiplot(TSS.plot,TTS.plot,CSS.plot,CTS.plot,ESS.plot,ETS.plot, cols=3)
+
+########### One Off charts ###########
+tmp.dec <- as.data.frame(decostand(tmp, 'standardize', MARGIN=2, na.rm =TRUE))
+tmp.dec$pos <- rownames(tmp.dec)
+#tmp.dec = cbind(Exposure,Treatment,tmp.dec)
+tmp.melt <- melt(tmp.dec, id=c("pos"))
+tmp.melt <- melt(tmp.dec, id=c("pos", "Exposure","Treatment"))
+
+tmp$pos <- rownames(tmp)
+tmp.melt <- melt(tmp, id=c("pos"))
+#######################################
+p <-ggplot(data=tmp.melt, aes(x=as.numeric(as.character(pos)), y=value, colour=variable)) # BASIC
+#p <-ggplot(data=tmp.melt, aes(x=as.numeric(as.character(variable)), y=value, colour=Exposure, lty=Treatment))
+p +
+  stat_smooth(method="loess", span=0.01, se=FALSE) +
+  #geom_line(aes(x=as.numeric(as.character(variable)), y=value)) +
+  scale_x_continuous(breaks = pretty_breaks(n=12)) +
+  scale_colour_brewer(palette="Paired") +
+  #geom_vline(0, colour="blue", lty=2) +
+  #scale_y_continuous(limits=c(-3.5, 3.5)) +
+  theme(legend.position = "bottom") +
+  labs(title = "TSS")
--- scripts/unfinished/3DGSP.py
+++ scripts/unfinished/3DGSP.py
@@ -4,13 +4,15 @@
 ## Daniel Pass | github.com/passdan | March 2016 ##
 ###################################################
 
-# Processing
+# Basic processing
 import argparse
 import pysam
+import re
+from collections import defaultdict
+# Numbers
 import numpy as np
 from scipy.stats.kde import gaussian_kde
 from scipy import stats
-from collections import defaultdict
 from sklearn.neighbors import KernelDensity
 # Graphing
 import plotly
@@ -20,29 +22,78 @@ import plotly.graph_objs as go
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Import Bam file and extract defined region for 2D or 3D plotting')
-    parser.add_argument('-i', required=True, help='Input Bam file')
+    parser.add_argument('-i', help='Input a Bam file or a list of comma-separated Bam files to merge (format: one.bam,two.bam,three.bam)')
     parser.add_argument('-o', help='Output prefix')
     parser.add_argument('-b', help='Regions to extract (Bed file)')
-    parser.add_argument('-B', help='Individual set of co-ordinates in Chr1,1000,1500 format (include commas)')
+    parser.add_argument('-B', help='Individual set of co-ordinates in bed format (`Chr1;1000-1510`) (include surrounding quotes to escape semi-colon)')
+    parser.add_argument('-g', help='Gene transcript to get positional information from | REQUIRES GTF FILE (-G)')
+    parser.add_argument('-G', help='gtf file to get gene model information from | REQUIRES GENE/TRANSCRIPT ID')
     parser.add_argument('-e', default=0, type=int, help='Integer for upstream/downstream extension')
     parser.add_argument('-q', default=10, type=int, help='Integer for rounding counts (size of fragment)')
     parser.add_argument('-r', default=50, type=int, help='Integer for rounding counts (base position)')
-    parser.add_argument('-p', default="3D", help='Choose plot type (2D or 3D, default:3D)')
+    parser.add_argument('-p', default="2D", help='Choose plot type (2D or 3D, default:2D)')
     parser.add_argument('-t', action='store_true', help='Use default co-ordinates for testing')
 
     args = parser.parse_args()
 
 def main():
-    # Load sam/bamfile
-    samfile = pysam.AlignmentFile(args.i, "rb")
+
+    sampleList = []
+    # If merging multiple bamfiles
+    if args.i:
+        samlist = args.i.split(',')
+        i = 1
+        for file in samlist:
+            print "Reading in file:" + file
+            f = pysam.AlignmentFile(file, "rb")
+            sampleList.append(f)
+            i += 1
+    else:
+        print "Need an input file! or use -h to see options"
+        quit()
+
+    # Get TSS/exon/UTR positions for gene model
+    geneID = args.g
+
+    # Define regions for extraction
+    boundD = defineRegions()
+
+    #kde = kdeTransform(poslist,sizelist)
+
+    if args.p == "2D":
+        globalX = []
+        globalY = []
+
+        # Force min/max
+        globalX.append(boundD['rangeStart'])
+        globalX.append(boundD['rangeEnd'])
+        globalY.append(51)
+        globalY.append(1000)
+
+        for samfile in sampleList:
+            print "Processing samfile"
+            simpleExtract(globalX,globalY,samfile,boundD)
+            samfile.close()
+
+        make2dHist(globalX,globalY,boundD)
+
+    elif args.p == "3D":
+        surfaceMat = regionExtract(samfile,chrom, regionStart, regionEnd)
+        samfile.close()
+
+        make3dPlot(surfaceMat, regionStart, regionEnd, regionLength)
+
+def defineRegions():
+
+    # Set dictionary for all boundaries
+    boundD = defaultdict(dict)
 
     # Load start/end regions
     if args.t is True:
         print "Using built in test co-ordinates: Chr1 16869721 16873927 | ~!NOT REAL RUN DATA!~ |"
-        chrom = 'Chr1'
-        regionStart = 16869721 - args.e
-        regionEnd = 16873927 + args.e
-        regionLength = regionEnd - regionStart
+        boundD['chrom'] = 'Chr1'
+        boundD['TSS'] = 16869721
+        boundD['TTS'] = 16873927
 
     elif args.b is None and args.B is None:
         print "Exiting as no co-ordinates given"
@@ -55,29 +106,18 @@ def main():
     elif args.B is not None:
         print "Setting extraction co-ordinates from command line"
 
-        bedParams = args.B.split(',')
-        chrom = bedParams[0]
-        regionStart = int(bedParams[1]) - args.e
-        regionEnd = int(bedParams[2]) + args.e
-        regionLength = regionEnd - regionStart
+        b1 = args.B.split(';')
+        boundD['chrom'] = b1[0]
+        b2 = b1[1].split('-')
+        boundD['TSS'] = int(b2[0])
+        boundD['TTS'] = int(b2[1])
 
+    boundD['glen'] = boundD['TSS'] - boundD['TTS']
+    boundD['rangeStart'] = boundD['TSS'] - args.e
+    boundD['rangeEnd'] = boundD['TTS'] + args.e
 
-    #cdsStart = 16869921
-
-    #kde = kdeTransform(poslist,sizelist)
-
-    if args.p == "2D":
-        (poslist,sizelist) = simpleExtract(samfile,chrom, regionStart, regionEnd)
-        samfile.close()
-
-        make2dHist(poslist,sizelist, regionStart, regionEnd, regionLength)
-
-    elif args.p == "3D":
-        surfaceMat = regionExtract(samfile,chrom, regionStart, regionEnd)
-        samfile.close()
-
-        make3dPlot(surfaceMat, regionStart, regionEnd, regionLength)
-
+    return boundD
+    #return(chrom,regionStart,regionEnd,regionLength)
 
 def regionExtract(samfile,chr,start,end):
 
@@ -121,32 +161,21 @@ def regionExtract(samfile,chr,start,end):
 
     return surfaceMat
 
-def simpleExtract(samfile,chr,start,end):
-    m1 = []
-    m2 = []
+def simpleExtract(globalX,globalY,samfile,boundD):
 
-    # Force min/max
-    m1.append(start)
-    m1.append(start)
-    m2.append(0)
-    m2.append(1000)
+    for read in samfile.fetch(boundD['chrom'],boundD['rangeStart'],boundD['rangeEnd']):
+        #pos = read.reference_start
+        #size = abs(read.template_length)
 
-    for read in samfile.fetch(chr, start, end):
-        pos = read.reference_start
-        size = abs(read.template_length)
-
-        #pos = int(round(read.reference_start, -1))
-        #size = int(round(abs(read.template_length), -1))
-
-        #pos = (int(read.reference_start / args.r)) * args.r
-        #size = (int(abs(read.template_length) / args.r)) * args.r
+        pos = (int(read.reference_start / args.r)) * args.r
+        size = (int(abs(read.template_length) / args.q)) * args.q
 
         while pos <= read.reference_end:
-            m1.append(pos)
-            m2.append(size)
+            globalX.append(pos)
+            globalY.append(size)
             pos += 1
 
-    return (m1,m2)
+    #return (m1,m2)
 
 def kdeTransform(poslist,sizelist):
     #data=numpy.random.multivariate_normal(mu,surfaceMat,1000)
@@ -165,6 +194,61 @@ def kdeTransform(poslist,sizelist):
     print kde
     return kde
 
+def buildGeneModel():
+    gtfFile = open(args.G, 'rb')
+    gene = args.g
+    geneModel = []
+    gmin = None
+    gmax = None
+
+    for line in gtfFile:
+        if re.search(gene, line):
+            gline = line.split('\t')
+            if gmin is None:
+                gmin = gline[3]
+                gmax = gline[3]
+
+            # Find min
+            if gline[3] < gmin:
+                gmin = gline[3]
+            elif gline[3] > gmax:
+                gmax = gline[3]
+
+            # Find max
+            if gline[4] < gmin:
+                gmin = gline[4]
+            elif gline[4] > gmax:
+                gmax = gline[4]
+
+            if re.search('UTR', gline[2]):
+                geneModel.append(
+                    {
+                    'type': 'rect',
+                    'x0':gline[3] ,'y0': 51,'x1':gline[4] ,'y1': 59,
+                    'line': {'color': 'rgb(0,0,0)','width': 2},
+                    'fillcolor': 'rgb(0,100,0)'
+                    }
+                )
+            if re.match('CDS', gline[2]):
+                geneModel.append(
+                    {
+                    'type': 'rect',
+                    'x0':gline[3] ,'y0': 51,'x1':gline[4] ,'y1': 59,
+                    'line': {'color': 'rgb(0,0,0)','width': 2},
+                    'fillcolor': 'rgb(0,0,0)'
+                    }
+                )
+    # Make full transcript line
+    geneModel.insert(0,
+        {
+        'type': 'rect',
+        'x0':gmin ,'y0': 52.5,'x1':gmax ,'y1': 57.5,
+        'line': {'color': 'rgb(0,0,0)','width': 2},
+        'fillcolor': 'rgb(100,0,0)'
+        }
+    )
+
+    return geneModel
 
 def make3dPlot(surfaceMatrix, regionStart, regionEnd, regionLength):
     upstream = (float(args.e) / regionLength) * 100
@@ -174,7 +258,7 @@ def make3dPlot(surfaceMatrix, regionStart, regionEnd, regionLength):
     layout = go.Layout(
         title=args.i,
         autosize=True,
-        width=1600,
+        width=1590,
         height=1000,
 
         scene=dict(
@@ -211,22 +295,22 @@ def make3dPlot(surfaceMatrix, regionStart, regionEnd, regionLength):
     outname = str(args.i) + "-3Dsurface.html"
     plotly.offline.plot(fig, filename=outname)
 
-def make2dHist(poslist, sizelist, regionStart, regionEnd, regionLength):
+def make2dHist(poslist, sizelist, boundD):
 
     trace1 = go.Histogram2dContour(
         x=poslist,
         y=sizelist,
-        contours=dict(
-            showlines=False
-        )
+        contours=dict(showlines=False),
+        colorscale=[[0, 'rgb(255,255,255)'], [0.25, 'rgb(31,120,180)'], [0.45, 'rgb(178,223,138)'], [0.65, 'rgb(51,159,44)'], [0.85, 'rgb(251,154,153)'], [1, 'rgb(227,26,28)']],
     )
-    data= go.Data([trace1])
+
+    data = go.Data([trace1])
 
     layout = go.Layout(
         title=args.i,
         autosize=True,
-        width=1600,
-        height=600,
+        width=1800,
+        height=800,
         yaxis=dict(
             title='Particle Size',
             titlefont=dict(
@@ -234,8 +318,11 @@ def make2dHist(poslist, sizelist, regionStart, regionEnd, regionLength):
                 size=18,
                 #color='lightgrey'
             ),
+            showgrid=True,
             type='log',
-            autorange=True
+            #autorange=True
+            # log 50 and log 500. Christ knows why it wont let me log it in place...
+            range=[1.698970004,2.698970004]
         ),
         xaxis=dict(
             title="Genomic Co-ordinates",
@@ -245,22 +332,54 @@ def make2dHist(poslist, sizelist, regionStart, regionEnd, regionLength):
                 #color='lightgrey'
             )
         ),
-        # shapes = [{
+        shapes = buildGeneModel()
+        # shapes = [
+        # {
+        #     'type': 'rect',
+        #     'x0': boundD['TSS'],'y0': 52.5,'x1': boundD['TTS'],'y1': 57.5,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2},
+        #     'fillcolor': 'rgb(100,0,0)'
+        # },
+        # {
+        #     'type': 'rect',
+        #     'x0':3625475 ,'y0': 51,'x1':3625619 ,'y1': 59,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2},
+        #     'fillcolor': 'rgb(000,0,0)'
+        # },
+        # {
+        #     'type': 'rect',
+        #     'x0':3625708 ,'y0': 51,'x1':3626294 ,'y1': 59,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2},
+        #     'fillcolor': 'rgb(000,0,0)'
+        # },
+        # {
+        #     'type': 'rect',
+        #     'x0':3626387 ,'y0': 51,'x1':3626488 ,'y1': 59,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2},
+        #     'fillcolor': 'rgb(000,0,0)'
+        # },
+        # {
+        #     'type': 'rect',
+        #     'x0':3626568 ,'y0': 51,'x1':3626938 ,'y1': 59,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2},
+        #     'fillcolor': 'rgb(000,0,0)'
+        # },
+        # {
+        #     'type': 'rect',
+        #     'x0':3627100 ,'y0': 51,'x1':3627139 ,'y1': 59,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2},
+        #     'fillcolor': 'rgb(000,0,0)'
+        # },
+        # {
         #     'type': 'line',
-        #     'x0': 16869721,
-        #     'y0': 50,
-        #     'x1': 16873927,
-        #     'y1': 50,
-        #     'line': {
-        #         'color': 'rgb(50, 171, 96)',
-        #         'width': 10,
-        #         'dash': 'dashdot',
-        #     }
-        # }]
+        #     'x0':boundD['rangeStart'] ,'y0': 60,'x1':boundD['rangeEnd'] ,'y1': 60,
+        #     'line': {'color': 'rgb(0,0,0)','width': 2}
+        # }
+        # ]
     )
 
     fig = go.Figure(data=data, layout=layout)
-    outname = str(args.i) + "-2Dcontour.html"
+    outname = str(args.o) + "-2Dcontour.html"
     plotly.offline.plot(fig, filename=outname)
 
 if __name__ == "__main__":
