cfb870bfa2118f388cecdaf47c851928e1829678
passdan <daniel.antony.pass@googlemail.com>
2018-08-31 17:55:38 +0100

    a

--- scripts/unfinished/ProbeTest.py
+++ scripts/unfinished/ProbeTest.py
@@ -11,8 +11,7 @@ from Bio.Blast.Applications import NcbiblastnCommandline
 if __name__ =='__main__':
     parser = argparse.ArgumentParser(description='Provide a region of DNA/RNA to be sliced to test for matches against a reference')
     parser.add_argument('-i', help='DNA segment you want to slice for probes (FASTA FORMAT)')
-    parser.add_argument('-w', default=21, help='Window length of the probes you want to test | default = 20')
-    parser.add_argument('-r', help='Reference fasta file that you want to test against (FASTA FORMAT)')
+    parser.add_argument('-w', default=21, help='Window length of the probes you want to test | default = 21')
     parser.add_argument('-d', help='Blast database you want to search against (make sure it is discoverable or give full path)')
     parser.add_argument('-a', default=1, help='Number of cores you want to blast with | default = 1')
 
@@ -20,23 +19,25 @@ if __name__ =='__main__':
 
 
 def main():
+    if args.i.endswith('.fasta'):
+        outputSlug = args.i[:-6]
+
     if args.i:
         with open(args.i, "rU") as handle:
             for siRNA in SeqIO.parse(handle, "fasta"):
                 probeArray = sliceProbe(siRNA)
                 print str(len(probeArray)) + " probes were generated"
-                probesFile =  str(args.i) + ".probes.fasta"
+                probesFile =  outputSlug + "-probes.fasta"
                 SeqIO.write(probeArray, probesFile, "fasta")
-
     else:
         parser.print_help()
         quit()
 
-    blastn(probesFile)
+    blastn(probesFile, outputSlug)
 
 
 def sliceProbe(siRNA):
-    print "Reading siRNA and sliceing it into windows of", args.w, "size"
+    print "Reading siRNA and slicing it into windows of", args.w, "size"
 
     probes = []
     i = 0
@@ -48,12 +49,13 @@ def sliceProbe(siRNA):
 
     return probes
 
-def blastn(probesFile):
+def blastn(probesFile, outputSlug):
     print("blasting probe sequence " + probesFile + " against the " + args.d + " database")
 
     blast_in = (probesFile)
-    blast_out = (probesFile + ".blast")
+    blast_out = (outputSlug + "-probes.blast")
 
+    # OUTPUT COLUMNS ARE: query id, subject id, % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score, sequence
     blastn_cline = NcbiblastnCommandline(task="blastn-short", query=blast_in, db=args.d, evalue=0.01, outfmt=6, out=blast_out, num_threads=args.a)
 
     stdout, stderr = blastn_cline()
@@ -67,39 +69,39 @@ def blastn(probesFile):
     return blast_out
 
 
-def primer2pwm(primer):
-    """
-    Write a primer sequence as a position weight matrix.
-    """
-    # Create 4 lists of length equal to primer's length.
-    matrix = [[0] * len(primer) for i in range(4)]
-    # List of correspondance IUPAC.
-    IUPAC = {
-        "A" : ["A"],
-        "C" : ["C"],
-        "G" : ["G"],
-        "T" : ["T"],
-        "U" : ["U"],
-        "R" : ["G", "A"],
-        "Y" : ["T", "C"],
-        "K" : ["G", "T"],
-        "M" : ["A", "C"],
-        "S" : ["G", "C"],
-        "W" : ["A", "T"],
-        "B" : ["C", "G", "T"],
-        "D" : ["A", "G", "T"],
-        "H" : ["A", "C", "T"],
-        "V" : ["A", "C", "G"],
-        "N" : ["A", "C", "G", "T"]
-    }
-    # Position of nucleotides in the PWM.
-    dico = {"A" : 0,  "C" : 1, "G" : 2, "T" : 3}
-    # Read each IUPAC letter in the primer.
-    for index, letter in enumerate(primer):
-        for nuc in IUPAC.get(letter):
-            i = dico.get(nuc)
-            matrix[i][index] = 1
-    return matrix
+# def primer2pwm(primer):
+#     """
+#     Write a primer sequence as a position weight matrix.
+#     """
+#     # Create 4 lists of length equal to primer's length.
+#     matrix = [[0] * len(primer) for i in range(4)]
+#     # List of correspondance IUPAC.
+#     IUPAC = {
+#         "A" : ["A"],
+#         "C" : ["C"],
+#         "G" : ["G"],
+#         "T" : ["T"],
+#         "U" : ["U"],
+#         "R" : ["G", "A"],
+#         "Y" : ["T", "C"],
+#         "K" : ["G", "T"],
+#         "M" : ["A", "C"],
+#         "S" : ["G", "C"],
+#         "W" : ["A", "T"],
+#         "B" : ["C", "G", "T"],
+#         "D" : ["A", "G", "T"],
+#         "H" : ["A", "C", "T"],
+#         "V" : ["A", "C", "G"],
+#         "N" : ["A", "C", "G", "T"]
+#     }
+#     # Position of nucleotides in the PWM.
+#     dico = {"A" : 0,  "C" : 1, "G" : 2, "T" : 3}
+#     # Read each IUPAC letter in the primer.
+#     for index, letter in enumerate(primer):
+#         for nuc in IUPAC.get(letter):
+#             i = dico.get(nuc)
+#             matrix[i][index] = 1
+#     return matrix
 
 
 if __name__ == "__main__":
