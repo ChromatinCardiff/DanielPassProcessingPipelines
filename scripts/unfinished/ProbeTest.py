e6f0c832a870afeb8c059937a52ca651c7062764
passdan <daniel.antony.pass@googlemail.com>
2018-09-03 11:25:17 +0100

    annotates

--- scripts/unfinished/ProbeTest.py
+++ scripts/unfinished/ProbeTest.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python
 
+## Written by Dr. Daniel Pass for IGEM-Cardiff 2018
+## https://github.com/passdan
+
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-#import MOODS
 import argparse
 from Bio.Blast.Applications import NcbiblastnCommandline
 
-
-
 if __name__ =='__main__':
+    ## This defines the command line parameters and puts it into an array called args
     parser = argparse.ArgumentParser(description='Provide a region of DNA/RNA to be sliced to test for matches against a reference')
-    parser.add_argument('-i', help='DNA segment you want to slice for probes (FASTA FORMAT)')
+    parser.add_argument('-i', required=True, help='[REQUIRED] | DNA segment you want to slice for probes (FASTA FORMAT)')
+    parser.add_argument('-d', required=True, help='[REQUIRED] | Blast database you want to search against (make sure it is discoverable or give full path)')
     parser.add_argument('-w', default=21, help='Window length of the probes you want to test | default = 21')
-    parser.add_argument('-d', help='Blast database you want to search against (make sure it is discoverable or give full path)')
     parser.add_argument('-a', default=1, help='Number of cores you want to blast with | default = 1')
 
     args = parser.parse_args()
 
-
 def main():
-    if args.i:
-        if args.i.endswith('.fasta'):
-            outputSlug = args.i[:-6]
-            
-        with open(args.i, "rU") as handle:
-            for siRNA in SeqIO.parse(handle, "fasta"):
-                probeArray = sliceProbe(siRNA)
-                print str(len(probeArray)) + " probes were generated"
-                probesFile =  outputSlug + "-probes.fasta"
-                SeqIO.write(probeArray, probesFile, "fasta")
-    else:
-        parser.print_help()
-        quit()
+    ## If the input file is a fasta (It should be!) then do this to get the main part of the name for the outputs, otherwise use the whole filename
+    if args.i.endswith('.fasta'):
+        outputSlug = args.i[:-6]
+    elif args.i.endswith('.fa'):
+        outputSlug = args.i[:-3]
+    else
+        outputSlug = args.i
 
+    ## Open the input fasta and read it in to memory
+    with open(args.i, "rU") as handle:
+        for siRNA in SeqIO.parse(handle, "fasta"):
+            ## Run the slicing method to get an array of the X-length fragments
+            probeArray = sliceProbe(siRNA)
+            ## Some nice outputs and write it to a file
+            print str(len(probeArray)) + " probes were generated"
+            probesFile =  outputSlug + "-probes.fasta"
+            SeqIO.write(probeArray, probesFile, "fasta")
+
+    ## Run blast on all the probes generated
     blastn(probesFile, outputSlug)
 
 
@@ -42,6 +47,7 @@ def sliceProbe(siRNA):
     probes = []
     i = 0
 
+    ## output the first 21 bases (or user specified) in fasta format (needed to go into blastn) then move one along base at a time, outputting each one
     while i <= len(siRNA.seq) - args.w:
         probeSeq = siRNA.seq[i:i+args.w]
         probes.append(SeqRecord(probeSeq, id=str(probeSeq), description=""))
@@ -50,11 +56,13 @@ def sliceProbe(siRNA):
     return probes
 
 def blastn(probesFile, outputSlug):
-    print("blasting probe sequence " + probesFile + " against the " + args.d + " database")
+    ## define the parameters
+    print("blasting probe sequence " + probesFile + " against the " + args.d + " database with BLASTN-SHORT")
 
     blast_in = (probesFile)
     blast_out = (outputSlug + "-probes.blast")
 
+    ## Define the command. NOTE: Running blastn-short!
     # OUTPUT COLUMNS ARE: query id, subject id, % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score, sequence
     blastn_cline = NcbiblastnCommandline(task="blastn-short", query=blast_in, db=args.d, evalue=0.01, outfmt=6, out=blast_out, num_threads=args.a)
 
@@ -67,42 +75,6 @@ def blastn(probesFile, outputSlug):
     blast_stdout_log.write(stdout)
 
     return blast_out
-
-
-# def primer2pwm(primer):
-#     """
-#     Write a primer sequence as a position weight matrix.
-#     """
-#     # Create 4 lists of length equal to primer's length.
-#     matrix = [[0] * len(primer) for i in range(4)]
-#     # List of correspondance IUPAC.
-#     IUPAC = {
-#         "A" : ["A"],
-#         "C" : ["C"],
-#         "G" : ["G"],
-#         "T" : ["T"],
-#         "U" : ["U"],
-#         "R" : ["G", "A"],
-#         "Y" : ["T", "C"],
-#         "K" : ["G", "T"],
-#         "M" : ["A", "C"],
-#         "S" : ["G", "C"],
-#         "W" : ["A", "T"],
-#         "B" : ["C", "G", "T"],
-#         "D" : ["A", "G", "T"],
-#         "H" : ["A", "C", "T"],
-#         "V" : ["A", "C", "G"],
-#         "N" : ["A", "C", "G", "T"]
-#     }
-#     # Position of nucleotides in the PWM.
-#     dico = {"A" : 0,  "C" : 1, "G" : 2, "T" : 3}
-#     # Read each IUPAC letter in the primer.
-#     for index, letter in enumerate(primer):
-#         for nuc in IUPAC.get(letter):
-#             i = dico.get(nuc)
-#             matrix[i][index] = 1
-#     return matrix
-
 
 if __name__ == "__main__":
     main()
